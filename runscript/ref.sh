bd1c9eda650cf09910516b75b429dc238a3e11c3
2channelkrt <2channelkrt@gmail.com>
2024-11-22 14:54:03 +0900

    add stuff

---
+++ runscript/ref.sh
@@ -0,0 +1,15 @@
+gem5_dir=~/code/temp/gem5/
+
+cmd=../binary/out.arm
+
+config_file=${gem5_dir}/configs/deprecated/example/se.py
+build=ARM
+
+out_dir="m5out-ref"
+
+debug_flags="--debug-flags=O3CPUAll,CacheAll,Event,PacketQueue,XBar"
+
+debug_file="ref-out.txt"
+debug_start="50000000"
+
+${gem5_dir}/build/${build}/gem5.opt ${debug_flags} -d ${out_dir} --debug-start ${debug_start} --debug-file ${debug_file} ${config_file} --cpu-type=O3CPU --caches --sys-clock="2.5GHz" --cpu-clock="2.5GHz" --cmd=${cmd} --mem-type="DDR4_2400_4x16"
